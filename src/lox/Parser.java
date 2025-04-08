1d38f37fe62fa8a4b79ac3c45116ec1bbe5d527f
durbanoutie <durbanout13@gmail.com>
2025-04-08 14:56:55 +0200

    Functions

--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -1,5 +1,6 @@
 package lox;
 
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -30,6 +31,9 @@ class Parser{
 
     private Stmt declaration(){
         try{
+            if(match(TokenType.FUN)){
+                return function("function");
+            }
             if(match(TokenType.VAR)){
                 return varDeclaration();
             }
@@ -49,6 +53,9 @@ class Parser{
         }
         if(match(TokenType.PRINT)){
             return printStatement();
+        }
+        if(match(TokenType.RETURN)){
+            return returnStatement();
         }
         if(match(TokenType.WHILE)){
             return whileStatement();
@@ -137,6 +144,19 @@ class Parser{
         return new Stmt.Print(value);
     }
 
+    private Stmt returnStatement(){
+
+        Token keyword = previous();
+        Expr value = null;
+
+        if(!check(TokenType.SEMICOLON)){
+            value = expression();
+        }
+        consume(TokenType.SEMICOLON, "Expect ';' after return value.");
+
+        return new Stmt.Return(keyword, value);
+    }
+
     private Stmt varDeclaration(){
         Token name = consume(TokenType.IDENTIFIER, "Expect variable name.");
 
@@ -163,6 +183,25 @@ class Parser{
         consume(TokenType.SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
     }
+
+    private Stmt.Function function(String kind){
+        Token name = consume(TokenType.IDENTIFIER, "Expect " + kind + " name.");
+        consume(TokenType.LEFT_PAREN, "Expect '(' after " + kind + " name.");
+        List<Token> parameters = new ArrayList<>();
+        if(!check(TokenType.RIGHT_PAREN)){
+            do{
+                if(parameters.size() >= 255){
+                    error(peek(), "Cant have more than 255 parameters.");
+                }
+                parameters.add(consume(TokenType.IDENTIFIER, "Expect parameter name."));
+            }while(match(TokenType.COMMA));
+        }
+        consume(TokenType.RIGHT_PAREN, "Expect ')' after parameters.");
+        consume(TokenType.LEFT_BRACE, "Expect '{' before " + kind + " body.");
+        List<Stmt> body = block();
+        return new Stmt.Function(name, parameters, body);
+    }
+
 
     private Expr assignment(){
         Expr expr = or();
@@ -255,7 +294,36 @@ class Parser{
             Expr right = unary();
             return new Expr.Unary(operator, right);
         }
-        return primary();
+        return call();
+    }
+    private Expr call(){
+        Expr expr = primary();
+
+        while(true){
+            if(match(TokenType.LEFT_PAREN)){
+                expr = finishCall(expr);
+            }else{
+                break;
+            }
+        }
+
+        return expr;
+    }
+
+    private Expr finishCall(Expr callee){
+        List<Expr> arguments = new ArrayList<>();
+        if(!check(TokenType.RIGHT_PAREN)){
+            do{
+                if(arguments.size() >= 255){
+                    error(peek(), "Cant have more than 255 arguments.");
+                }
+                arguments.add(expression());
+            } while(match(TokenType.COMMA));
+        }
+
+        Token paren = consume(TokenType.RIGHT_PAREN, "Expect ')' after arguments.");
+
+        return new Expr.Call(callee, paren, arguments);
     }
 
     private Expr primary(){
