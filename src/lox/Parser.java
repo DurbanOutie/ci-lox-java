c64c9f8c8b4fcbcb679c9e595b2946102ec5ba46
durbanoutie <durbanout13@gmail.com>
2025-04-11 23:54:44 +0200

    Inheritance

--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -49,6 +49,11 @@ class Parser{
 
     private Stmt classDeclaration(){
         Token name = consume(TokenType.IDENTIFIER, "Expect class name.");
+        Expr.Variable superclass = null;
+        if(match(TokenType.LESS)){
+            consume(TokenType.IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
         consume(TokenType.LEFT_BRACE, "Expect '{' before class body.");
         List<Stmt.Function> methods = new ArrayList<>();
         while(!check(TokenType.RIGHT_BRACE) && !isAtEnd()){
@@ -56,7 +61,7 @@ class Parser{
         }
 
         consume(TokenType.RIGHT_BRACE, "Expect '}' after class body.");
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Stmt statement(){
@@ -357,6 +362,12 @@ class Parser{
         }
         if(match(TokenType.NIL)){
             return new Expr.Literal(null);
+        }
+        if(match(TokenType.SUPER)){
+            Token keyword = previous();
+            consume(TokenType.DOT, "Expect '.' after 'super'.");
+            Token method = consume(TokenType.IDENTIFIER, "Expect superclass method name.");
+            return new Expr.Super(keyword, method);
         }
         if(match(TokenType.THIS)){
             return new Expr.This(previous());
